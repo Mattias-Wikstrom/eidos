a025e376cda249180a57b4f3fca39225086dad58
Mattias Wikström <mattias@rexuniversal.com>
2026-05-09 17:54:37 +0200

    Removing unused files

--- src/Eidos/Backend/CoqProps/CoqAxiomSet.hs
+++
@@ -1,8 +0,0 @@
--- | Re-export of 'Eidos.Pipeline.AxiomSet'.
---
--- Mirrors 'Eidos.Backend.LeanProps.LeanAxiomSet' for the Coq backend.
-module Eidos.Backend.CoqProps.CoqAxiomSet
-  ( module Eidos.Pipeline.AxiomSet
-  ) where
-
-import Eidos.Pipeline.AxiomSet
